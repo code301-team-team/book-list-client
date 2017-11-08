dbff03294907668539e60de865d04b2b40ae6a3e
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-11-08 10:29:13 -0800

    fresh start

---
+++ scripts/models/book.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let app = app || {};
+// TODO How do I make this dynamic?
+let __API_URL__ = 'http://127.0.0.1:3000';
+
+((mpdule) => {
+
+  function errorCallback(err) {
+    console.log('ReadApp.fetch')
+  }
+
+})
