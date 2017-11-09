bdcfdae2ae4a81cad6efccb8ec2406193dfa9945
dataluxe <phelan.carey@gmail.com>
2017-11-09 10:19:59 -0800

    Added client request testDatabase to return books from heroku DB

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,9 +2,15 @@
 
 let __API_URL__ = 'https://fr-pc-dm-booklist.herokuapp.com';
 
-let getMessage = ()=> {
+let getMessage = () => {
   console.log('Requesting data from server.js');
   $.get(`${__API_URL__}/test`)
     .done((data) => {console.log(data)})
-    .fail(()=> {console.log('failure')})
+    .fail(()=> {console.log('Failure in Proof of Life')})
+}
+
+let testDatabase = () => {
+  $.get(`${__API_URL__}/api/v1/books`)
+  .done((data) => {console.log(data)})
+  .fail(()=> {console.log('Failure connecting to database.')})
 }
