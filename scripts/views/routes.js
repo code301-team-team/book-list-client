5721bea02c3dfc808603e632957c0e69b9a6aa85
dataluxe <phelan.carey@gmail.com>
2017-11-10 17:40:33 -0800

    More test changes mark 3

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,5 +1,5 @@
 'use strict';
-page('/book-list-client/', ctx => app.Book.fetchAll(ctx, app.bookListView.initIndexPage));
+page('/book-list-client/', () => app.Book.fetchAll(app.bookListView.initIndexPage));
 page('/book-list-client/books:book_id', ctx => {
   app.Book.fetchOne(ctx, app.bookDetailView.initIndexPage)
 });
